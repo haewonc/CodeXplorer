913f2291a4128ceffe524d080d3bdf30705e4dca
stdeguzman <seandeguzman0742@gmail.com>
2023-11-26 23:46:35 +0900

    search bar

--- my-app/src/Components/TabBar.jsx
+++ my-app/src/Components/TabBar.jsx
@@ -3,6 +3,9 @@ import "../stylesheets/tabBar.css";
 const TabBar = (activeFile) => {
     return (
     <div className="tabBar">
+    <div style={{ textAlign: 'right' }}> 
+        <input type="text" id="search" name="search" placeholder="Enter your search query..."></input>
+    </div>
     <p className="tabActive">{activeFile['activeFile']}</p>
     </div>
  );
